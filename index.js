20b9378fc50a5cd9a826dbb8c1ac2f22ec2ff694
Lisa Lyayuga <lisalyayuga@gmail.com>
2022-08-12 17:01:39 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML="<h1>Lisa is the champion</h1>"
